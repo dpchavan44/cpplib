a5c7a4cd7753d057791b00e4abe5a73c2f3c7efc
dpchavan44 <dattaprasadchavan4@gmail.com>
2026-03-03 16:48:59 +0530

    Delete design_pattern/factory_method directory

--- design_pattern/factory_method/VehicleInterface.hxx
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class VehicleInterface
-{
-public:
-    virtual ~VehicleInterface() = default; // virtual destructor for proper clean up of dervied classes
-    virtual void drive() = 0;
-    // force all vehicle to implement drive method
-};
